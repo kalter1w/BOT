3bb6ebe847b250b5c0469ff1e788f4164bd53bb7
kalter1w <37780671+kalter1w@users.noreply.github.com>
2019-06-17 21:41:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const TelegramBot = require ('node-telegram-bot-api');
 const request = require('request');
-require ('dotenv').config()
-const token = process.env.BOT_TOKEN;
+
+
+const token = $BOT_TOKEN;
 const bot = new TelegramBot (token, {polling: true});
 
 
