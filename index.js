18edb2362d96df12d8babc352a69916a3ed064bd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-27 12:39:38 +0000

    Deploy kalter1w/BOT to github.com/kalter1w/BOT.git:gh-pages

---
+++ index.js
@@ -0,0 +1,52 @@
+const TelegramBot = require ('node-telegram-bot-api');
+const request = require('request');
+
+
+const token = $BOT_TOKEN;
+const bot = new TelegramBot (token, {polling: true});
+
+
+
+bot.onText(/\/curse/, (msg, match) => {
+  const chatId = msg.chat.id;
+
+  bot.sendMessage(chatId, 'Выберите нужную валюту', {
+    reply_markup: {
+      inline_keyboard: [
+        [
+          {
+          text: 'EUR',
+          callback_data: 'EUR'
+        },  {
+          text: 'USD',
+          callback_data: 'USD'
+        },{
+          text: 'RUB',
+          callback_data: 'RUB'
+        },{
+          text:'BTC',
+          callback_data: 'BTC'
+        }
+        ]
+      ]
+
+
+
+    }
+  });
+});
+
+
+bot.on('callback_query', query => {
+  const id = query.message.chat.id;
+  request('https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5', function (error, response, body) {
+    const data = JSON.parse(body);
+    const result = data.filter(item => item.ccy === query.data)[0];
+    let md = `
+    *${result.ccy} => ${result.base_ccy}*
+    Buy: _${result.buy}_
+    Sale: _${result.sale}_
+    `;
+    bot.sendMessage(id,md, {parse_mode:'Markdown'});
+  })
+})
